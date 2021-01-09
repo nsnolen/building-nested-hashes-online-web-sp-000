e43bb507c32247499c77b618e2b659a066ee007e
Sara Nolen <nolensara1@gmail.com>
2021-01-09 16:31:25 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -36,7 +36,7 @@ def first_challenge
          :age => "51"
        },
        :hero => {
-         :name => "Juliet"
+         :name => "Juliet",
          :age => "15"
        },
        :hero_friends => [
